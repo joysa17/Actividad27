78d953ffad02266d2aa83a1c7a280599da8d9fa8
joysa <joselin.solis.3b@gmail.com>
2017-11-08 19:26:07 -0300

    Ejercicio completado

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -9,7 +9,12 @@ class ProductsController < ApplicationController
 			redirect_to category_path(params[:category_id])
 		end
 	end 
-	
+
+	def destroy
+		@product = Product.find(params[:id]).destroy
+		redirect_to category_path(@product.category_id)
+	end
+
 	private
 	def product_params
 		params.require(:product).permit(:name, :price)
